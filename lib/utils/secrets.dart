02e9d42f290e2bd763fa9206b56c8692f0800ddc
Augustus <onyekachiaugustus111@gmail.com>
2023-12-05 22:48:17 +0100

    secrete file removed

--- lib/utils/secrets.dart
+++
@@ -1,5 +0,0 @@
-class Secrets {
-  // Add your Google Maps API Key here
-  static const aPIKEY = 'AIzaSyDs9RvfkIH_6qy_13HO8aGpaIGta7yEPCc';
-  static const aKey =    "AIzaSyDs9RvfkIH_6qy_13HO8aGpaIGta7yEPCc";
-}
